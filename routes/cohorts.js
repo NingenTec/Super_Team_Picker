92300372e0b5d93ec9a81fb14a25e93696cc0f88
NingenTech <59954804+NingenTec@users.noreply.github.com>
2020-07-03 12:31:02 -0700

    add route and views logic

--- routes/cohorts.js
+++ routes/cohorts.js
@@ -3,6 +3,30 @@ const express = require("express");
 const router = express.Router();
 const knex = require("../db");
 
+// Team Assignments logic
+const shuffle = (arr) => {
+  const { floor, random } = Math;
+  // Array.from clones the input array to avoid mutation
+  return Array.from(arr).sort(() => [-1, 1][floor(random() * 2)]);
+};
+
+const assignTeams = (method, quantity, members) => {
+  const teams = [];
+  const shuffleMembers = shuffle(members);
+  const max = members.length;
+  const teamCount =
+    method === "teamCount" ? quantity : Math.ceil(max / quantity);
+
+  for (let i = 0; i < max; i += 1) {
+    const id = i % teamCount;
+    teams[id] = teams[id]
+      ? teams[id].concat([shuffleMembers[i]])
+      : [shuffleMembers[i]];
+  }
+
+  return teams;
+};
+
 // ROUTES
 // Index
 router.get("/", (req, res) => {
@@ -12,6 +36,7 @@ router.get("/", (req, res) => {
       res.render("cohorts/index", { cohorts });
     });
 });
+
 // Create
 router.post("/", (req, res) => {
   const { name, members, logoUrl } = req.body;
@@ -22,10 +47,30 @@ router.post("/", (req, res) => {
     .returning("id")
     .then((id) => res.redirect(`/cohorts/${id}`));
 });
+
 // New
-router.get("/new", (req, res) => {
+router.get("/new", (req, res, next) => {
   res.render("cohorts/new");
 });
+
 // Show
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const { method, quantity } = req.query;
+
+  knex("cohorts")
+    .first()
+    .where({ id })
+    .then((cohort) => {
+      let teams = [];
+      if ([method, quantity, cohort.members].every((x) => !!x)) {
+        const members = cohort.members.split(/,\s*/);
+        teams = assignTeams(method, quantity, members);
+      }
+
+      res.render("cohorts/show", { cohort, teams, method, quantity });
+    })
+    .catch((err) => next(err));
+});
 
 module.exports = router;
