0406bb304190173a235b43b5b8ef4cd4a872d200
NingenTech <59954804+NingenTec@users.noreply.github.com>
2020-07-03 11:49:00 -0700

    Add routes and create a cohort

--- routes/cohorts.js
+++ routes/cohorts.js
@@ -13,9 +13,19 @@ router.get("/", (req, res) => {
     });
 });
 // Create
+router.post("/", (req, res) => {
+  const { name, members, logoUrl } = req.body;
 
+  knex
+    .insert({ name, members, logoUrl })
+    .into("cohorts")
+    .returning("id")
+    .then((id) => res.redirect(`/cohorts/${id}`));
+});
 // New
-
+router.get("/new", (req, res) => {
+  res.render("cohorts/new");
+});
 // Show
 
 module.exports = router;
